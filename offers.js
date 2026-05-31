d75d24aa58a99b897e57e82023a7f90f26eb1281
Sunavlis-corp <sundaysilvanus166@gmail.com>
2026-05-31 04:35:20 +0100

    Modify offers array with new URLs
    Updated the offers array to include duplicate URLs.

--- offers.js
+++ offers.js
@@ -1,5 +1,5 @@
 const offers = [
-    "https://omg10.com/4/11024651",
     "https://www.yongsocial.com/",
-    "https://www.tradewill.com/"
+    "https://www.yongsocial.com/",
+    "https://www.yongsocial.com/"
 ];
